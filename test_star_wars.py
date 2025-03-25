edfd24f31710c75206786792e543bafd50772a1b
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-25 08:57:31 +0100

    Test to get the first tow letters of the first name

--- test_star_wars.py
+++ test_star_wars.py
@@ -3,3 +3,12 @@ from star_wars import star_wars_name
 
 def test_star_wars_name_exists():
     assert star_wars_name
+
+
+def test_two_letters_first_name():
+    name = star_wars_name(
+        'Dan van der Jackson',
+        'O\'Brien',
+        'Edinburgh'
+    )
+    assert name == 'Da'
