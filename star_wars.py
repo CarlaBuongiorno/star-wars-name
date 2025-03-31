cb619f2977e01c5ae46990e3bc03a990b4c51a22
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-31 15:27:31 +0200

    Refactor tests with parametrize, and refactor star_wars.py

--- star_wars.py
+++ star_wars.py
@@ -1,9 +1,8 @@
 def star_wars_name(name, maiden_name, city):
-    f_name = get_star_wars_first_name(name)
-    return f'{f_name}'
+    starwars_name = get_star_wars_first_name(name)
+    return f'{starwars_name}'
 
 
 def get_star_wars_first_name(name):
     full_name = name.split()
-    starwars_f_name = f'{full_name[-1][:3]}{full_name[0][:2].lower()}'
-    return starwars_f_name
+    return f'{full_name[-1][:3]}{full_name[0][:2].lower()}'
--- test_star_wars.py
+++ test_star_wars.py
@@ -1,3 +1,5 @@
+import pytest
+
 from star_wars import star_wars_name
 
 
@@ -5,46 +7,12 @@ def test_star_wars_name_exists():
     assert star_wars_name
 
 
-def test_one_letter_name():
-    name = star_wars_name(
-        'D W',
-        '',
-        ''
-    )
-    assert name == 'Wd'
-
-
-def test_two_letter_name():
-    name = star_wars_name(
-        'Da Wu',
-        '',
-        ''
-    )
-    assert name == 'Wuda'
-
-
-def test_three_letter_first_name():
-    name = star_wars_name(
-        'Dan Jac',
-        '',
-        ''
-    )
-    assert name == 'Jacda'
-
-
-def test_four_letter_first_name():
-    name = star_wars_name(
-        'John Jac',
-        '',
-        ''
-    )
-    assert name == 'Jacjo'
-
-
-def test_four_letter_last_name():
-    name = star_wars_name(
-        'John Jack',
-        '',
-        ''
-    )
-    assert name == 'Jacjo'
+@pytest.mark.parametrize('name, maiden_name, city, excpected', [
+     ('D W', '', '',                        'Wd'),
+     ('Da Wu', '', '',                      'Wuda'),
+     ('Dan Jac', '', '',                    'Jacda'),
+     ('John Jac', '', '',                   'Jacjo'),
+     ('Dan van der Jackson', '', '',        'Jacda'),
+])
+def test_star_wars_name(name, maiden_name, city, excpected):
+    assert star_wars_name(name, maiden_name, city,) == excpected
