05430ba9ec5789fa669aba656fd2f409e1e251fa
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-04-10 21:52:14 +0200

    Fix index out of range error and refactor

--- star_wars.py
+++ star_wars.py
@@ -1,19 +1,21 @@
 def star_wars_name(name, maiden_name, city):
     cleaned_maiden_name = _clean_input(maiden_name)
-    starwars_f_name = get_star_wars_first_name(name)
-    starwars_l_name = get_star_wars_last_name(cleaned_maiden_name, city)
-    return f'{starwars_f_name} {starwars_l_name}'.title()
+    full_name = get_star_wars_first_name(name)
+    if not maiden_name and not city:
+        return f'{full_name}'.title()
+    return f'{full_name} {cleaned_maiden_name[:2]}{city[:3]}'.title()
 
 
 def _clean_input(user_input):
-    return ''.join(i for i in user_input if i.isalnum() or '')
+    if len(user_input.split()) > 1:
+        user_input = user_input[-1]
+        return ''.join(i for i in user_input if i.isalnum())
+    return ''.join(i for i in user_input if i.isalnum())
+
 
 
 def get_star_wars_first_name(name):
-    if name:
-        full_name = name.split()
+    full_name = name.split()
+    if len(full_name) > 1:
         return f'{full_name[-1][:3]}{full_name[0][:2]}'
-    return name
-
-def get_star_wars_last_name(maiden_name, city):
-    return f'{maiden_name[:2]}{city[:3]}'
+    return ''.join(full_name)
--- test_star_wars.py
+++ test_star_wars.py
@@ -7,21 +7,23 @@ def test_star_wars_name_exists():
     assert star_wars_name
 
 
-@pytest.mark.parametrize('name, maiden_name, city, excpected', [
-     ('', '', '',                           ' '),
-     ('D W', '', '',                        'Wd '),
-     ('Da Wu', '', '',                      'Wuda '),
-     ('Dan Jac', '', '',                    'Jacda '),
-     ('John Jac', '', '',                   'Jacjo '),
-     ('Dan van der Jackson', '', '',        'Jacda '),
-     ('D W', 'O', '',                       'Wd O'),
-     ('D W', 'Obr', '',                     'Wd Ob'),
-     ('D W', 'O\'br', '',                   'Wd Ob'),
-     ('D W', 'O\'Br', '',                   'Wd Ob'),
-     ('D W', 'O\'Brien', 'E',               'Wd Obe'),
-     ('D W', 'O\'Br', 'Edinburgh',          'Wd Obedi'),
-     ('D W', '', 'Edinburgh',               'Wd Edi'),
-     ('Dan van der Jackson', 'O\'Brien', 'Edinburgh','Jacda Obedi'),
+@pytest.mark.parametrize('name, maiden_name, city, expected', [
+     ('', '', '',                                       ''),
+     ('D', '', '',                                       'D'), # First name
+     ('D W', '', '',                                    'Wd'),
+     ('Da Wu', '', '',                                  'Wuda'),
+     ('Dan Jac', '', '',                                'Jacda'),
+     ('John Jac', '', '',                               'Jacjo'),
+     ('Dan van der Jackson', '', '',                    'Jacda'),
+     ('D W', 'O', '',                                   'Wd O'),
+     ('D W', 'Obr', '',                                 'Wd Ob'),
+     ('D W', 'O\'br', '',                               'Wd Ob'),
+     ('D W', 'O\'Br', '',                               'Wd Ob'),
+     ('D W', 'O\'Brien', 'E',                           'Wd Obe'),
+     ('D W', 'O\'Br', 'Edinburgh',                      'Wd Obedi'),
+     ('D W', '', 'Edinburgh',                           'Wd Edi'),
+     ('Dan van der Jackson', 'O\'Brien', 'Edinburgh',   'Jacda Obedi'),
+    #  ('Dan Jackson', 'van der Jackson', 'Edinburgh',    'Jacda Jaedi'),
 ])
-def test_star_wars_name(name, maiden_name, city, excpected):
-    assert star_wars_name(name, maiden_name, city,) == excpected
+def test_star_wars_name(name, maiden_name, city, expected):
+    assert star_wars_name(name, maiden_name, city,) == expected
