1ef8c44c60ef5ea7377bcabe9b7b68e2bc60132b
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-04-22 12:41:45 +0200

    Refactor further

--- star_wars.py
+++ star_wars.py
@@ -1,27 +1,32 @@
 def star_wars_name(name, maiden_name, city):
-    cleaned_maiden_name = _clean_maiden_name(maiden_name)
-    star_wars_first_name = get_star_wars_first_name(name)
+    star_wars_first_name = _get_star_wars_first_name(name)
+    star_wars_last_name = _get_star_wars_last_name(maiden_name, city)
     if not maiden_name and not city:
         return f'{star_wars_first_name}'.title()
-    return f'{star_wars_first_name} {cleaned_maiden_name[:2]}{city[:3]}'.title()
+    return f'{star_wars_first_name} {star_wars_last_name}'.title()
 
 
-def _clean_maiden_name(maiden_name):
-    split_name = maiden_name.split()
-    if len(split_name) > 0:
-        return ''.join(i for i in split_name[-1] if i.isalnum())
-    else:
-        return ''
-
-
-def get_star_wars_first_name(name):
+def _get_star_wars_first_name(name):
     full_name = name.split()
-    return f'{get_part_from_last_name(full_name)}{get_part_from_first_name(full_name)}'
+    return f'{_get_part_from_last_name(full_name)}{_get_part_from_first_name(full_name)}'
 
 
-def get_part_from_first_name(split_full_name):
+def _get_part_from_last_name(split_full_name):
+    return split_full_name[-1][:3] if len(split_full_name) > 1 else ''
+
+
+def _get_part_from_first_name(split_full_name):
     return split_full_name[0][:2] if len(split_full_name) > 0 else ''
 
 
-def get_part_from_last_name(split_full_name):
-    return split_full_name[-1][:3] if len(split_full_name) > 1 else ''
+def _get_star_wars_last_name(maiden_name, city):
+    split_maiden_name = maiden_name.split()
+    return f'{_get_part_from_maiden_name(split_maiden_name)}{_get_part_from_city(city)}'
+
+
+def _get_part_from_maiden_name(split_maiden_name):
+    return (''.join(i for i in split_maiden_name[-1] if i.isalnum()))[:2] if len(split_maiden_name) > 0 else ''
+
+
+def _get_part_from_city(city):
+    return city[:3]
